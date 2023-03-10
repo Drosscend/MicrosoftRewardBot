71fd60cff896f6c345e2ead8e1a2ef0e1bdc8a09
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-03-10 18:58:21 +0100

    fix(search): fix de la recherche et allègement

--- getGoogleTrend.js
+++ getGoogleTrend.js
@@ -1,34 +1,59 @@
-const baseURL = `https://trends.google.com`;
-const countryCode = "FR";
-const category = "all";
+export class GoogleTrends {
 
-const fillTrendsDataFromPage = async (page) => {
-    // charger la page
-    while (true) {
-        const isNextPage = await page.$(".feed-load-more-button");
-        if (!isNextPage) break;
-        await page.click(".feed-load-more-button");
-        await page.waitForTimeout(2000);
+    constructor() {
+        this.baseURL = `https://trends.google.com`;
+        this.countryCode = "FR";
+        this.category = "all";
     }
-    const dataFromPage = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".feed-item")).map((item) => {
-            const title = item.querySelector(".title");
-            const query = title.innerText.replace(/[^a-zA-Z0-9 ]/g, "");
-            return { query };
-        } );
-    }, baseURL);
-    return dataFromPage;
+
+    /**
+     * Fill the trends data from the page
+     * @param page - The puppeteer page
+     * @returns {Promise<*>} - A promise that resolves with the trends data
+     */
+    async fillTrendsDataFromPage(page) {
+        // Charger la page
+        const maxTrends = 40;
+        let trends = [];
+
+        while (trends.length < maxTrends) {
+            // Récupérer les tendances actuelles
+            const currentTrends = await page.evaluate(() => {
+                return Array.from(document.querySelectorAll(".feed-item")).map((item) => {
+                    const title = item.querySelector(".title");
+                    const query = title.innerText.replace(/[^a-zA-Z0-9 ]/g, "");
+                    return {query};
+                });
+            });
+
+            // Ajouter les tendances actuelles à la liste totale des tendances
+            trends = [...trends, ...currentTrends];
+
+            // Vérifier s'il reste des tendances à charger
+            const isNextPage = await page.$(".feed-load-more-button");
+            if (!isNextPage || trends.length >= maxTrends) break;
+
+            // Charger plus de tendances
+            await page.click(".feed-load-more-button");
+            await page.waitForTimeout(2000);
+        }
+
+        // Récupérer les 40 premières tendances
+        return trends.slice(0, maxTrends);
+    }
+
+    /**
+     * Get Google Trends
+     * @param page - The puppeteer page
+     * @returns {Promise<*>} - A promise that resolves with the Google Trends
+     */
+    async getGoogleTrends(page) {
+        const URL = `${this.baseURL}/trends/trendingsearches/realtime?geo=${this.countryCode}&category=${this.category}&hl=fr`;
+
+        await page.goto(URL);
+        await page.waitForSelector(".feed-item");
+
+        return await this.fillTrendsDataFromPage(page);
+    }
+
 }
-
-getTrends = async (page) => {
-    const URL = `${baseURL}/trends/trendingsearches/realtime?geo=${countryCode}&category=${category}&hl=fr`;
-
-    await page.goto(URL);
-    await page.waitForSelector(".feed-item");
-
-    return await fillTrendsDataFromPage(page);
-}
-
-module.exports = {
-    getTrends
-};
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-puppeteer.use(StealthPlugin());
-const dotenv = require('dotenv')
+import dotenv from 'dotenv';
+import puppeteer from 'puppeteer-extra';
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+// Add adblocker plugin to block all ads and trackers (saves bandwidth)
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+import {getEdgePath} from "edge-paths";
+import {GoogleTrends} from "./getGoogleTrend.js";
+
 dotenv.config();
 
-const google = require('./getGoogleTrend.js');
+puppeteer.use(StealthPlugin())
+
+puppeteer.use(AdblockerPlugin({blockTrackers: true}))
 
 const REWARD_PAGE_URL = 'https://rewards.bing.com/';
 const BING_USERNAME = process.env.BING_USERNAME;
@@ -19,47 +26,51 @@ const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
  * Make research on Bing
- * @param {Page} page - The page to use
+ * @param client - The puppeteer client
  * @param {string} query - The query to search
  */
-const search = async (page, query) => {
+const search = async (client, query) => {
     console.log(`Searching for ${query}`);
-    await page.goto(`https://www.bing.com/search?q=${query}`);
+    await client.goto(`https://www.bing.com/search?q=${query}`);
     await wait(1000);
 };
 
-const bingLogin = async (page) => {
-    await page.type('#i0116', BING_USERNAME);
-    await page.click('#idSIButton9');
-    await page.waitForSelector('#i0118');
-    await page.type('#i0118', BING_PASSWORD);
+/**
+ * Login to Bing
+ * @param client - The puppeteer client
+ * @returns {Promise<void>} - A promise that resolves after the login
+ */
+const bingLogin = async (client) => {
+    await client.goto(REWARD_PAGE_URL);
+    await client.type('#i0116', BING_USERNAME);
+    await client.click('#idSIButton9');
+    await client.waitForSelector('#i0118');
+    await client.type('#i0118', BING_PASSWORD);
     await wait(500);
-    await page.click('#idSIButton9');
-    await page.waitForNavigation();
-    await page.click('#idSIButton9');
-    await page.waitForNavigation();
+    await client.click('#idSIButton9');
+    await client.waitForNavigation();
     console.log('Logged in');
 };
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      });
-    const page = await browser.newPage();
-    page.setViewport({ width: 1200, height: 700 });
-    await page.setDefaultNavigationTimeout(60000);
+puppeteer.launch({
+    headless: false,
+    ignoreDefaultArgs: ['--disable-extensions'],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath: getEdgePath()
+}).then(async browser => {
+    const Page = await browser.newPage();
+    await Page.setViewport({width: 1200, height: 700});
+    await Page.setDefaultNavigationTimeout(60000);
 
-    // await page.goto(REWARD_PAGE_URL);
+    //Login
+    await bingLogin(Page);
 
-    // Login
-    // await bingLogin(page);
-
-    const googleTrendTab = await google.getTrends(page)
-
+    // Get Google Trend
+    const googleTrends = new GoogleTrends();
+    const googleTrendTab = await googleTrends.getGoogleTrends(Page);
     for (let i = 0; i < googleTrendTab.length; i++) {
-        await search(page, googleTrendTab[i].query);
+        await search(Page, googleTrendTab[i].query);
     }
 
     await browser.close();
-})();
+});
